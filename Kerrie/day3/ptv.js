b745f6d862459501001aaf92f14d072f0b118095
Kerrie Batrouney <batrouney@gmail.com>
2016-09-29 15:52:12 +1000

    seems to work now

--- Kerrie/day3/ptv.js
+++ Kerrie/day3/ptv.js
@@ -5,8 +5,8 @@ var sandringham = ['Sandringham', 'Southern Cross', 'Richmond', 'South Yarra', '
 var trainLines = [alamein, glenwaverley, sandringham];
 
 
-var origin = 'Burnley';
-var destination = 'Flagstaff';
+var origin = 'Glenferrie';
+var destination = 'Richmond';
 
 
 //get origin and destination
@@ -34,28 +34,8 @@ for (var i = 0; i < trainLines.length; i++) {
 //  }
 }
 }
-/*
-if (condition1) {
-    block of code to be executed if condition1 is true
-} else if (condition2) {
-    block of code to be executed if the condition1 is false and condition2 is true
-} else {
-    block of code to be executed if the condition1 is false and condition2 is false
-}
-*/
 
 
-
-
-//if origin or dest is Richmond, then simple journey.... but find the correct Richmond (on the correct line)
-/*if (origin = "Richmond") {
-  originLine = desLine;
-
-}
-if (destination = 'Richmond') {
-  desLine = originLine;
-}
-*/
 /*
 //checks if station is on network
 if (originLocIndex === null) {
@@ -97,13 +77,22 @@ if (originLine[0] === desLine[0]) {
 else if (originLine[0] !== desLine[0]) {
   //different line, must change at Richmond
   // get the 2 legs
-  if (originLocIndex<originLine.indexOf("Richmond")) {
-    var leg1 = originLine.slice(originLocIndex, originLine.indexOf("Richmond"));
+ if (originLocIndex < originLine.indexOf("Richmond")) {
+    var leg1 = originLine.slice(originLocIndex, originLine.indexOf("Richmond")+1);
 } else {
-  var leg1 = originLine.slice(originLine.indexOf("Richmond"), originLocIndex);
+  var leg1 = originLine.slice(originLine.indexOf("Richmond"), originLocIndex +1);
 }
+if (leg1[0]=== "Richmond") {
+  leg1.reverse();
+
+}
+
   if (destLocIndex < desLine.indexOf("Richmond")){
-    var leg2 = desLine.slice(destLocIndex, desLine.indexOf("Richmond"));
+    var leg2 = desLine.slice(destLocIndex, desLine.indexOf("Richmond")+1);
+    if (leg2[0] !== "Richmond") {
+      leg2 = leg2.reverse();
+    }
+
 } else {
     var leg2 = desLine.slice(desLine.indexOf("Richmond"), destLocIndex);
 }
@@ -143,7 +132,7 @@ function printRoute2(routeArray) {
 //console.log((array.slice(start, end).concat(array2.slice(start,end))).join('---->'));
   console.log('Change at Richmond');
   console.log(routeArray.join(' ----> '));
-  console.log('number of stops - ' + routeArray[routeArray.length-1]);
+  console.log('number of stops - ' + (routeArray.length-1));
 }
 /*origin: Melbourne Central
 destination: Richmond
